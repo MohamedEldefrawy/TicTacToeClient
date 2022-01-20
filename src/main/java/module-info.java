fd69f726f7cf3d87d7d33154a0d0d87d815bc04b
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-01-20 22:08:50 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.client.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.client.client to javafx.fxml;
+    exports com.client.client;
+}
