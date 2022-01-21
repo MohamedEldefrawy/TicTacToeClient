5364c1678b6a3ca8b22b306c01800cf837df2f46
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-01-21 19:29:56 +0200

    creating resources css,images,views  packages

--- src/main/java/com/client/client/HelloApplication.java
+++ src/main/java/com/client/client/HelloApplication.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        System.out.println(getClass().getResource());
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
