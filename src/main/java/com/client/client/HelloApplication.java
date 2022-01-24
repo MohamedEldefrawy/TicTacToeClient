9959ef7a0a6c8f368bfb27d5a7a6a2c54b86b3b1
MohamedYehia <mohamed.yehia331@gmail.com>
2022-01-24 13:02:01 +0200

    created loggin view and controller

--- src/main/java/com/client/client/HelloApplication.java
+++ src/main/java/com/client/client/HelloApplication.java
@@ -1,6 +1,8 @@
 package com.client.client;
 
+import javafx.scene.Node;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,14 +10,43 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+       // FXMLLoader loginFxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+       // Scene loginScene = new Scene(loginFxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
     }
+    public void switchToLoginScene(ActionEvent e) throws IOException{
+        FXMLLoader loginFxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/login.fxml"));
+        Scene loginScene = new Scene(loginFxmlLoader.load(), 400, 400);
+        Stage st = (Stage)((Node)e.getSource()).getScene().getWindow();
+        st.setScene(loginScene);
+    }
+    public void switchToGameFirstScene(ActionEvent e) throws  IOException{
+        FXMLLoader gameFirstFxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/gameFirstScene.fxml"));
+        Scene gameFirstScene = new Scene(gameFirstFxmlLoader.load(), 400, 400);
+        Stage st = (Stage)((Node)e.getSource()).getScene().getWindow();
+        st.setScene(gameFirstScene);
+    }
+    public  void switchToSignUpScene(ActionEvent e) throws IOException{
+        FXMLLoader signUpFxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/signUp.fxml"));
+        Scene signUpScene = new Scene(signUpFxmlLoader.load(), 400, 400);
+        Stage st = (Stage)((Node)e.getSource()).getScene().getWindow();
+        st.setScene(signUpScene );
+
+
+    }
+
+
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/controllers/HelloController.java
+++ src/main/java/controllers/HelloController.java
@@ -1,9 +1,16 @@
 package controllers;
 
+import com.client.client.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,12 +20,18 @@ public class HelloController implements Initializable {
     public Label welcomeText;
 
 
-    public void btnHelloClicked() {
-        welcomeText.setText("Hello World !!!!");
+    public void btnHelloClicked(ActionEvent e) {
+       // welcomeText.setText("Hello World !!!!");
+        HelloApplication obj = new HelloApplication();
+        try {
+            obj.switchToLoginScene(e);
+        }catch (IOException ex){ex.printStackTrace();}
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        btnHello.setOnAction(actionEvent -> btnHelloClicked());
+        btnHello.setOnAction(actionEvent -> btnHelloClicked(actionEvent));
+
+
     }
 }
