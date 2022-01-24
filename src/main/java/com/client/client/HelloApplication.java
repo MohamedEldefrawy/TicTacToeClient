560a9e01a39a4122b230508da779438542b9dd6f
Mahitab Mohsen <mahitabmohsen6@gamil.com>
2022-01-24 13:35:26 +0200

    creating ui's for client side

--- src/main/java/com/client/client/HelloApplication.java
+++ src/main/java/com/client/client/HelloApplication.java
@@ -10,10 +10,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/client/views/hello-view.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("/com/client/views/sign in.fxml"));
+        Scene scene1 = new Scene(fxmlLoader1.load(), 600, 400);
+        FXMLLoader fxmlLoader2 = new FXMLLoader(getClass().getResource("/com/client/views/difficulty.fxml"));
+        Scene scene2 = new Scene(fxmlLoader2.load(), 600, 400);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(scene2);
         stage.show();
     }
 
