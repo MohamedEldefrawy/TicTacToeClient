65755cc6675f4c7aac53da6f008a1a4954417e95
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-01-27 17:30:04 +0200

    fix login bug

--- src/main/java/connections/ConnectionHandler.java
+++ src/main/java/connections/ConnectionHandler.java
@@ -6,16 +6,19 @@ import model.Dtos.userDtos.LogoutUserDto;
 import model.Dtos.userDtos.RegisterUserDto;
 import services.UserService;
 import utilities.JsonBuilder;
+import utilities.Singleton;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketException;
 
 public class ConnectionHandler {
     private Socket socket;
-    private BufferedReader reader;
-    private BufferedWriter writer;
+    private DataInputStream reader;
+    private DataOutputStream writer;
     private UserService userService;
 
     public ConnectionHandler() {
@@ -28,8 +31,8 @@ public class ConnectionHandler {
     private void establishConnection() {
         try {
             socket = new Socket(InetAddress.getLocalHost(), 5005);
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            reader = new DataInputStream(socket.getInputStream());
+            writer = new DataOutputStream(socket.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -61,10 +64,13 @@ public class ConnectionHandler {
 
     private void responseHandler(String jsonString) {
         JsonObject response = JsonBuilder.toJsonObject(jsonString);
-        switch (response.get("operation").toString()) {
-            case "login":  // Handle logic after login
+        Singleton singleton = Singleton.getInstance();
+        switch (response.get("operation").getAsString()) {
+            case "login":
+                singleton.setLoginStatus(response.get("result").getAsBoolean());
+                System.out.println(singleton.getLoginStatus());
                 break;
-            case "regiset":// handle login after register
+            case "register":// handle login after register
                 break;
             case "logout": // handle logic after logout
                 break;
@@ -73,15 +79,12 @@ public class ConnectionHandler {
 
     // Client listener thread
     public class ServerListener extends Thread {
-        public ServerListener() {
-            start();
-        }
-
         @Override
         public void run() {
             while (socket.isConnected()) {
                 try {
-                    String response = reader.readLine();
+                    String response = reader.readUTF();
+                    System.out.println(response);
                     responseHandler(response);
                 } catch (SocketException e) {
                     e.printStackTrace();
