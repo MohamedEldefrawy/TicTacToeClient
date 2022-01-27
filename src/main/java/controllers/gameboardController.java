263c5b8d33d96fdc7adb42481b9f348701e83724
Mahitab Mohsen <mahitabmohsen6@gamil.com>
2022-01-27 11:13:16 +0200

    ui client side edit

--- src/main/java/controllers/gameboardController.java
+++ src/main/java/controllers/gameboardController.java
@@ -1,5 +1,27 @@
 package controllers;
 
-public class gameboardController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
+import java.beans.EventHandler;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class gameboardController implements Initializable {
+
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+    public Button btn5;
+    public Button btn6;
+    public Button btn7;
+    public Button btn8;
+    public Button btn9;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    btn1.setOnAction();
+    }
 }
